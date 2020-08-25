1805e3971aa9efade1e3d020c206505d877501ea
brainiac295 <chicxulubimpactor@outlook.com>
2020-08-25 21:07:13 +0530

    Update Draft

--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace AmiBrokerExecutor
 {
     class Program
     {
+        public static string APIKey = "";
+        public static string OrderUrl = "";
+        public static int QTY = 25;
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            PlaceOrder(APIKey, QTY, "s");
+        }
+
+        public static void PlaceOrder(string Line,int QTY,string Type)
+        {
+
         }
     }
 }
