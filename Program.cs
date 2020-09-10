3e85c09b0f5d13f08fd925f9be321b85746cd2ef
brainiac295 <chicxulubimpactor@outlook.com>
2020-09-10 08:59:43 +0530

    Updateww

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 
 namespace AmiBrokerExecutor
@@ -17,7 +21,30 @@ namespace AmiBrokerExecutor
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");         
+            Console.WriteLine("Hello World");
+            string Type = args[3];
+            string Line = "";
+
+            string[] Files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            foreach (string F in Files)
+            {
+                if (F.Contains(".txt"))
+                {
+                    int StartIndex = F.LastIndexOf("\\");
+                    StartIndex++;
+                    string Str = F.Substring(StartIndex);
+                    Str = Str.Replace(".txt", "");
+                    Line = Str;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Token:" + Line);
+
+            if (args.Count()>=4)
+            {
+                
+            }
         }
 
         public static void PlaceOrder(string Line,int QTY,string Type)
